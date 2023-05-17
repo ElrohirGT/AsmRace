3afc59ac79f8876f2c7e3b360e2cbb6e3f455fd7
Daniel Rayo <ray22933@uvg.edu.gt>
2023-05-17 10:33:49 -0600

    Feat: Add functions to generate random numbers

--- AsmRace/main.asm
+++ AsmRace/main.asm
@@ -5,3 +5,28 @@
 ; Daniel Rayo 22933
 ; Flavio Galán 22386
 ; Description: Juego de carreras en MASM para x86.
+.386
+.model flat, stdcall, c
+.stack 4096
+
+includelib libucrt.lib
+includelib legacy_stdio_definitions.lib
+includelib libcmt.lib
+includelib libvcruntime.lib
+
+extrn printf:near
+extrn scanf:near
+extrn exit:near
+extrn srand:near
+extrn rand:near
+
+.data
+
+.code
+
+public main
+main proc
+
+
+main endp
+end
