634641804cdd39aceac97a8bdae127ac0940b5f1
Daniel Rayo <ray22933@uvg.edu.gt>
2023-05-18 18:30:31 -0600

    Feat: Now numbers can be generate between a custom boundary

--- AsmRace/main.asm
+++ AsmRace/main.asm
@@ -5,6 +5,7 @@
 ; Daniel Rayo 22933
 ; Flavio Galán 22386
 ; Description: Juego de carreras en MASM para x86.
+
 .386
 .model flat, stdcall, c
 .stack 4096
@@ -21,24 +22,58 @@ extrn srand:near
 extrn rand:near
 
 .data
-	randomSeed DWORD ?
-	message BYTE "Insert your lucky number: " 0, 0
-
-	digitFormat "%d", 0, 0
-
-	call  generateRandomNumber
+	randomSeed		DWORD 0
+	randomNum		DWORD ?
+	RANDOM_MAX		DWORD 7
+	message			BYTE "Insert your lucky number: ", 0, 0
+	digitFormat		BYTE "%d", 0, 0
 
 .code
 
 main PROC
+;	--- PREPARE STACK ---
+	push ebp
+	mov ebp, esp
+
+;   --- SETTING RANDOM ---
 	call generateRandomNumber
-main ENDP
+
+;	--- CLEAN STACK ---
+	add esp, 0
+	push 0
+	call exit
+
+	RET
 
 generateRandomNumber PROC
-	push message
+;		Let the user input a seed, generate a random number
+;		between 1 and RANDOM_MAX (exclusive).
+
+	push offset message
 	call printf
+	add esp, 4
+
+	lea eax, randomSeed
+	push eax
+	push offset digitFormat
+	call scanf
+	add esp, 8
+
+	push randomSeed
+	call srand
+	call rand
+	add esp, 4
+
+	mov ebx, RANDOM_MAX
+	SUB EDX, EDX
+	div ebx
+
+	inc edx
+	mov randomNum, edx
+
 	RET
 
 generateRandomNumber ENDP
 
-end
+main ENDP
+	END
