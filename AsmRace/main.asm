5dbcb6560eab04705b2045d245056c33f3be84b7
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2023-05-23 18:21:10 -0600

    literalmente solo los carnets xd

--- AsmRace/main.asm
+++ AsmRace/main.asm
@@ -1,9 +1,9 @@
 ; Universidad del Valle de Guatemala
 ; Organización de Computadoras y Assembler
-; Angela García
-; Fernando Echeverría
-; Daniel Rayo 22933
-; Flavio Galán 22386
+; Angela García, 22869
+; Fernando Echeverría, 22610
+; Daniel Rayo, 22933
+; Flavio Galán, 22386
 ; Description: Juego de carreras en MASM para x86.
 
 .386
